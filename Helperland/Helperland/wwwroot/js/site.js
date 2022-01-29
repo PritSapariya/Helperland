08def3a9c424cb6c4e6a277ff6a758f16a0f573b
PritSapariya <pritsapariya17@gmail.com>
2022-01-29 19:34:28 +0530

    Integrated Backend in dotnet mvc and changes in some Frontend

---
+++ Helperland/Helperland/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(() => {
+
+    /* Sidenavbar Events Starts */
+    $('#hamburger').click(() => {
+        $('#side-navbar-model').addClass('open');
+    });
+    $('#side-navbar-wrapper').click(() => {
+        $('#side-navbar-model').removeClass('open');
+    });
+    $('#side-navbar-menu').click((e) => {
+        e.stopPropagation();
+    });
+    /* Sidenavbar Events Ends */
+
+    /* Window Events */
+    $(this).scroll(() => {
+        $('#side-navbar-model').removeClass('open');
+    });
+
+});
